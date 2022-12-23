dc05afe2d58990ff823dda1f6eb9cc3d23e7bb82
Rachel Wong <rwrachelwong@gmail.com>
2022-12-23 09:32:56 -0800

    create apolloclient, app, header

--- client/index.js
+++ client/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-client";
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import ReactDOM from "react-dom";
+import { Router, hashHistory, IndexRoute, Route } from "react-router";
+import App from "./components/App";
+
+const client = new ApolloClient({
+  dataIdFromObject: (o) => o.id,
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}></Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector("#root"));
+
+// hashHistory vs BrowserHistory
+// hashhistory a lot more flexible. don't have to worry about the correct set up
